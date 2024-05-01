7c1266b90e6a7a88576cd8e3979525f4f93fecdd
Darkskub <Darkskub@gmail.com>
2024-05-01 21:47:30 +0300

    Task #3

--- Python_HomeWork/main.py
+++ Python_HomeWork/main.py
@@ -1,17 +1,2 @@
-# Создаем пустой список
-l = []
-
-# Получаем количество элементов списка от пользователя
-count = int(input("Введите количество элементов в списке: "))
-
-# Получаем элементы списка от пользователя
-i = 0
-while len(l) < count:
-    element = input(f"Введите {len(l) + 1}-й элемент списка: ")
-    if not element.isdigit():
-        print("Вы ввели не число")
-        continue
-    l.append(int(element))
-    i += 1
-
-print("Список:", l)
+a = list(input())
+print(a[0].isdigit)
