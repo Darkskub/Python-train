5de7066e361a56e500bbf8f758dda717e7964890
Darkskub <Darkskub@gmail.com>
2024-05-07 20:17:47 +0300

    Task #6

--- Python_HomeWork/Task #5 2v/1.1.py
+++ Python_HomeWork/Task #5 2v/1.1.py
@@ -2,10 +2,10 @@ import random
 
 list_el = [100, 400, 500, 10, 50, "Банан", "Клубника", "Камень", "Морковь", "Огурец", "Пицца"]
 
-def random(list_el):
-    el1 = random.choice(list_el)
-    list_el.remove(el1)
-    el2 = random.choice(list_el)
+def random_elements(list_elements):
+    el1 = random.choice(list_elements)
+    list_elements.remove(el1)
+    el2 = random.choice(list_elements)
     return el1, el2
 
-print(random(list_el))
+print(random_elements(list_el))
---
+++ Python_HomeWork/Task #6/1.py
@@ -0,0 +1,24 @@
+def average_num(list_num: list) -> float:
+    for ind, el in enumerate(list_num):
+        if not isinstance(el, int | float):
+            try:
+                list_num[ind] = int(el)
+            except:
+                return "Bad request"
+    return round(sum(list_num)/len(list_num), 2)
+
+assert average_num([1, 2, 3, 4, 5]) == 3.00
+
+assert average_num([1.0, 2.5, 3.0, 4.5]) == 2.75
+
+assert average_num([1, 2.5, 3, 4.5]) == 2.75
+
+assert average_num([0, 0, 0, 0]) == 0.00
+
+assert average_num([-1, -2, -3, -4]) == -2.50
+
+assert average_num(["1", "2", "3", "4"]) == 2.50
+
+assert average_num([1, "2", 3.0, "4.5"]) == 2.63
+
+assert average_num([1, "two", 3, "four"]) == "Bad request"
